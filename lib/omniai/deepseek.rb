98d50e52f3f62779027b998427096c8cd826a962
Kevin Sylvestre <kevin@ksylvest.com>
2025-01-30 10:19:35 -0800

    Setup

---
+++ lib/omniai/deepseek.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "event_stream_parser"
+require "omniai"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.push_dir(__dir__, namespace: OmniAI)
+loader.inflector.inflect "deepseek" => "DeepSeek"
+loader.setup
+
+module OmniAI
+  # A namespace for everything DeepSeek.
+  module DeepSeek
+    # @return [OmniAI::DeepSeek::Config]
+    def self.config
+      @config ||= Config.new
+    end
+
+    # @yield [OmniAI::DeepSeek::Config]
+    def self.configure
+      yield config
+    end
+  end
+end
---
+++ lib/omniai/deepseek/chat.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module OmniAI
+  module DeepSeek
+    # An DeepSeek chat implementation.
+    #
+    # Usage:
+    #
+    #   completion = OmniAI::DeepSeek::Chat.process!(client: client) do |prompt|
+    #     prompt.system('You are an expert in the field of AI.')
+    #     prompt.user('What are the biggest risks of AI?')
+    #   end
+    #   completion.choice.message.content # '...'
+    class Chat < OmniAI::Chat
+      JSON_RESPONSE_FORMAT = { type: "json_object" }.freeze
+
+      module Model
+        CHAT = "deepseek-chat"
+        REASONER = "deepseek-reasoner"
+      end
+
+      DEFAULT_MODEL = Model::CHAT
+
+    protected
+
+      # @return [Hash]
+      def payload
+        OmniAI::DeepSeek.config.chat_options.merge({
+          messages: @prompt.serialize,
+          model: @model,
+          stream: @stream.nil? ? nil : !@stream.nil?,
+          temperature: @temperature,
+          response_format: (JSON_RESPONSE_FORMAT if @format.eql?(:json)),
+          tools: (@tools.map(&:serialize) if @tools&.any?),
+        }).compact
+      end
+
+      # @return [String]
+      def path
+        "/chat/completions"
+      end
+    end
+  end
+end
