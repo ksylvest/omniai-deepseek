6c72198239d40e3cd8c91ab2e7d173ad76e747ed
Kevin Sylvestre <kevin@ksylvest.com>
2025-03-03 13:33:16 -0800

    Support streaming ToolCall

--- spec/omniai/deepseek/chat_spec.rb
+++ spec/omniai/deepseek/chat_spec.rb
@@ -128,6 +128,7 @@ RSpec.describe OmniAI::DeepSeek::Chat do
 
       let(:prompt) { "Tell me a story." }
       let(:stream) { proc { |chunk| } }
+      let(:chunks) { [] }
 
       before do
         stub_request(:post, "https://api.deepseek.com/chat/completions")
@@ -139,18 +140,14 @@ RSpec.describe OmniAI::DeepSeek::Chat do
             stream: !stream.nil?,
           })
           .to_return(body: <<~STREAM)
-            data: #{JSON.generate({ choices: [{ delta: { role: 'assistant', content: 'A' } }] })}\n
-            data: #{JSON.generate({ choices: [{ delta: { role: 'assistant', content: 'B' } }] })}\n
+            data: #{JSON.generate({ choices: [{ index: 0, delta: { role: 'assistant', content: 'Hello' } }] })}\n
+            data: #{JSON.generate({ choices: [{ index: 0, delta: { role: 'assistant', content: ' ' } }] })}\n
+            data: #{JSON.generate({ choices: [{ index: 0, delta: { role: 'assistant', content: 'World' } }] })}\n
             data: [DONE]\n
           STREAM
       end
 
-      it do
-        chunks = []
-        allow(stream).to receive(:call) { |chunk| chunks << chunk }
-        completion
-        expect(chunks.map(&:text)).to eql(%w[A B])
-      end
+      it { expect(completion.text).to eql("Hello World") }
     end
 
     context "when using files / URLs" do
